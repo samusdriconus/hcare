1beca497e86fdac73bec7a1f60459257debb3371
Saidi Oussama <saidi-ouss@outlook.com>
2018-09-27 14:46:55 +0200

    adding paginator for patients view

--- HCare/patient_followup/views.py
+++ HCare/patient_followup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Patient
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -7,5 +8,14 @@ def home(request):
 	return render(request, 'home.html')
 
 def patients(request):
-    patients = Patient.objects.all()
+    patient_list = Patient.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(patient_list, 10)
+    try:
+        patients = paginator.page(page)
+    except PageNotAnInteger:
+        patients = paginator.page(1)
+    except EmptyPage:
+        patients = paginator.page(paginator.num_pages)
+    paginator = Paginator(patient_list, 10)
     return render(request, 'patients.html',{'patients':patients})
